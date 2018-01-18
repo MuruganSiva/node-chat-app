6d6845f58560fdff563d0b551199341b311a9d60
Siva Murugan <Siva@Sivas-MacBook-Pro.local>
2018-01-17 21:00:46 -0500

    Connection events

--- server/server.js
+++ server/server.js
@@ -1,14 +1,26 @@
 const path = require('path');
 const express = require('express');
+const socketio = require('socket.io');
+const http = require('http');
 
 var publicPath = path.join(__dirname, '../public');
 var port = process.env.PORT || 3000;
 
 var app = express();
+var server = http.createServer(app);
+var io = socketio(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) =>{
+    console.log('New user connected');
+
+    socket.on('disconnect', () =>{
+        console.log('Client disconnected');
+    });
+});
+
+server.listen(port, () => {
     console.log(`App is running on port ${port}`);
 });
 
