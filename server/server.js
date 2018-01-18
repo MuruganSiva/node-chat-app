72033d92fe8af3424f7bc0da7b83512ccf42f894
Siva Murugan <Siva@Sivas-MacBook-Pro.local>
2018-01-17 19:37:46 -0500

    initial commit

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const express = require('express');
+
+var publicPath = path.join(__dirname, '../public');
+var port = process.env.port || 3000;
+
+var app = express();
+
+app.use(express.static(publicPath));
+
+app.listen(port, () => {
+    console.log(`App is running on port ${port}`);
+});
+
+
